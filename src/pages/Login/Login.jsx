be5fdb7cd49057c05e7f8272bf4ee6f07e5c1e16
Sagor Ahamed <sagorahamed251245@gmail.com>
2023-05-18 18:15:22 +0600

    login and singup page setup

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,48 @@
+import { Link } from "react-router-dom";
 
 
 const Login = () => {
     return (
-        <h1>This is login</h1>
+        <div className="hero min-h-screen bg-base-200   font-bold">
+            <div className="hero-content flex-col-reverse lg:flex-row-reverse">
+                <div className="text-center lg:text-left flex items-center flex-col md:flex-row">
+
+                    <img className="md:w-[50%]" src="herologo-1.png" alt="" />
+                    <h1 className="text-6xl text-center font-bold text-white">Let's Go <span className="text-primary">Hero</span></h1>
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <from className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text text-white">Email</span>
+                            </label>
+                            <input name='name' type="text" placeholder="email" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text text-white">Password</span>
+                            </label>
+                            <input name='email' type="email" placeholder="password" className="input input-bordered" />
+
+                        </div>
+                        <div className=" mt-6">
+                            <input type="submit" className="btn btn-primary w-full text-white font-bold" value="Login"></input>
+                            <input type="submit" value="" />
+                        </div>
+
+                        <div className='mb-5 mt-3'>
+                            <button className="w-full text-white bg-sky-700 hover:bg-sky-800 focus:ring-4 focus:outline-none focus:ring-sky-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-sky-600 dark:hover:bg-sky-700 dark:focus:ring-sky-800">
+                                Login With Google</button>
+                        </div>
+                        <div className="text-sm font-medium text-white">
+                            Not registered? <Link to='/singup' className="text-primary hover:underline dark:text-green-500">Create account</Link>
+                        </div>
+                    </from>
+
+
+                </div>
+            </div>
+        </div>
     );
 };
 
--- src/pages/SingUp/SingUp.jsx
+++ src/pages/SingUp/SingUp.jsx
@@ -1,9 +1,59 @@
+import { Link } from "react-router-dom";
 
 
 const SingUp = () => {
     return (
-        <div>
-            <h1>This is singup</h1>
+        <div className="hero min-h-screen bg-base-200   font-bold">
+            <div className="hero-content flex-col-reverse lg:flex-row-reverse">
+                <div className="text-center lg:text-left flex items-center flex-col md:flex-row">
+
+                    <img className="md:w-[50%]" src="herologo-1.png" alt="" />
+                    <h1 className="text-6xl text-center font-bold text-white">Time To Save The World <span className="text-primary">Hero</span></h1>
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <from className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text text-white">Name</span>
+                            </label>
+                            <input name='name' type="text" placeholder="Name" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text text-white">Email</span>
+                            </label>
+                            <input name='email' type="email" placeholder="email" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text text-white">Password</span>
+                            </label>
+                            <input name='password' type="password" placeholder="password" className="input input-bordered" />
+
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text text-white">Photo Url</span>
+                            </label>
+                            <input name='photo_url' type="text" placeholder="Url" className="input input-bordered" />
+                        </div>
+                        <div className=" mt-6">
+                            <input type="submit" className="btn btn-primary w-full text-white font-bold" value="Sing UP"></input>
+                            <input type="submit" value="" />
+                        </div>
+
+                        <div className='mb-5 mt-3'>
+                            <button className="w-full text-white bg-sky-700 hover:bg-sky-800 focus:ring-4 focus:outline-none focus:ring-sky-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-sky-600 dark:hover:bg-sky-700 dark:focus:ring-sky-800">
+                                Sing up With Google</button>
+                        </div>
+                        <div className="text-sm font-medium text-white">
+                            Have an account? <Link to='/login' className="text-primary hover:underline dark:text-green-500">Login</Link>
+                        </div>
+                    </from>
+
+
+                </div>
+            </div>
         </div>
     );
 };
