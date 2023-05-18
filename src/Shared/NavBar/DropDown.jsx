35ec850f2b59e5bc2e8a9bdb31f1a6ebbbf92842
Sagor Ahamed <sagorahamed251245@gmail.com>
2023-05-18 11:50:42 +0600

    navbar done

---
+++ src/Shared/NavBar/DropDown.jsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+
+
+const DropDown = () => {
+    return (
+        <div className="dropdown  text-white font-bold">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+            </label>
+            <ul tabIndex={0} className="bg-primary menu menu-compact dropdown-content mt-3 p-2  shadow  rounded-box w-52">
+                <li><Link>Home</Link></li>
+                <li><Link>All Toys</Link></li>
+                <li><Link>Blogs</Link></li>
+                <li><Link>My Toys</Link></li>
+                <li><Link>Login</Link></li>
+                <li><Link>SingUp</Link></li>
+                <li><Link>Logout</Link></li>
+
+                <div className="avatar">
+                    <div className="w-10 mask mask-squircle">
+                        <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                    </div>
+                </div>
+            </ul>
+        </div>
+    );
+};
+
+export default DropDown;
--- src/Shared/NavBar/NavBar.jsx
+++ src/Shared/NavBar/NavBar.jsx
@@ -1,9 +1,43 @@
+import { Link } from "react-router-dom";
+import DropDown from "./DropDown";
 
 
 const NavBar = () => {
     return (
-        <div>
-            
+        <div className="navbar  font-bold text-white">
+            <div className="navbar-start">
+                <DropDown></DropDown>
+                <a className=" normal-case text-xl font-bold hidden lg:inline-block">Mah Heroes</a>
+                <img className="w-14 lg:inline-block hidden" src="herologo-1.png" alt="" />
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    <li><Link>Home</Link></li>
+                    <li><Link>All Toys</Link></li>
+                    <li><Link>Blogs</Link></li>
+                    <li><Link>My Toys</Link></li>
+
+
+                </ul>
+            </div>
+            <div className="navbar-end ">
+                <a className=" normal-case text-xl font-bold lg:hidden">Mah Heroes</a>
+                <img className="w-14 lg:hidden" src="herologo-1.png" alt="" />
+
+               <div className="hidden lg:inline-flex">
+               <ul className="menu menu-horizontal px-1 ">
+                    <li><Link>Login</Link></li>
+                    <li><Link>SingUp</Link></li>
+                    <li><Link>Logout</Link></li>
+                    <div className="avatar online">
+                        <div className="w-10 mask mask-squircle">
+                            <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                        </div>
+                    </div>
+
+                </ul>
+               </div>
+            </div>
         </div>
     );
 };
