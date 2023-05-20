2f41aee0945bb20ffbb5e2c8538247f497465e00
Sagor Ahamed <sagorahamed251245@gmail.com>
2023-05-20 22:34:34 +0600

    some modification

--- src/pages/Error/ErrorPage.jsx
+++ src/pages/Error/ErrorPage.jsx
@@ -5,7 +5,7 @@ const ErrorPage = () => {
     return (
         <div className="bg-primary h-screen flex items-center justify-center flex-col">
             <img className="" src="/public/error-404.gif" alt="" />
-            <p ><span className="text-3xl font-bold text-base-100">Go Back To </span><Link className="text-sky-500 font-bold">Home Page</Link></p>
+            <p ><span className="text-3xl font-bold text-white">Go Back To </span><Link><button className="btn font-bold text-black">Home Page</button></Link></p>
         </div>
     );
 };
